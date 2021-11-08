d8c6e7400b278ee81ee797ea168e2967b95889c4
Joe Loach <joe.gloach@gmail.com>
2021-11-08 03:33:33 +0000

    add the SDF trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,16 @@
 
 pub mod math;
 
+use math::*;
+
+/// Signed Distance Field.
+/// 
+/// Used to compute the signed distance from a point in space.
+pub trait Sdf {
+    /// The signed distance from point `p`.
+    fn dist(&self, p: &Vec3s) -> f32s;
+}
+
 /// The math prelude.
 /// 
 /// Imports useful types, functions and traits.
